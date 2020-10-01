5808a5e9083be8c68d7bc24ef181934d645007b0
msund385 <ms43004foru@gmail.com>
2020-09-30 21:49:37 -0400

    init commit

---
+++ Desktop/repos/InheritanceExercise/InheritanceExercise/Animal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceExercise
+{
+    public class Animal
+    {
+        public Animal()
+        {
+
+        }
+
+        public bool IsAlive { get; set; }
+        public int Age { get; set; }
+        public int LegCount { get; set; }
+        public string LandSeaAir { get; set; }
+
+
+    }         
+
+           
+
+
+
+
+
+
+
+
+         
+            
+           
+            
+            
+            
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  
+}
